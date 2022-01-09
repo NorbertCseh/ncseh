59d7db0eb95f8e08dc888c9f3c600d40028ac347
NorbertCseh <cseh.norbert94@gmail.com>
2022-01-09 23:14:02 +0100

    layout fix

--- src/sections/experience/index.tsx
+++ src/sections/experience/index.tsx
@@ -2,8 +2,8 @@ import Experience from "../../components/experience";
 
 const ExperienceSection = () => {
     return (
-        <section className="h-screen" id="experience">
-            <div className="flex-col m-auto">
+        <section className="flex m-auto h-screen" id="experience">
+            <div className="flex-col m-auto w-full">
                 <Experience date="2020 Jan - Today" companyAndRole="LogMeIn - Software developer engineer in test" tasks={["Creating automated test with TypeScript and WebdriverIO", "Documentation with TestRail", "Manual testing"]} />
                 <Experience date="2020 Apr - 2020 Dec" companyAndRole="KPMG - Quality Assurance Engineer" tasks={["Test automation with selenium", "Manual testing", "Documentation"]} />
                 <Experience date="2018 Nov - 2020 Mar" companyAndRole="Circeo - Test engineer" tasks={["Automatization with selenium with java", "Test documentation with Testrail", "Manual testing"]} />
