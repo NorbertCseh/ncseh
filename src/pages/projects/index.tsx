cab5da1727763143a7d72caf0b0ee82fd84fa696
NorbertCseh <cseh.norbert94@gmail.com>
2022-01-16 22:56:54 +0100

    basic layout for projects

---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,11 @@
+import NextPage from "next";
+
+const Projects: NextPage = () => {
+  return (
+    <section className="w-full h-screen flex justify-center items-center">
+      Projects
+    </section>
+  );
+};
+
+export default Projects;
---
+++ src/pages/projects/sorting.tsx
@@ -0,0 +1,11 @@
+import NextPage from "next";
+
+const Sorting: NextPage = () => {
+  return (
+    <section className="w-full h-screen flex justify-center items-center">
+      Sorting
+    </section>
+  );
+};
+
+export default Sorting;
