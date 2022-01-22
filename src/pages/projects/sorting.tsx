eedcc86fd3216b8b99231a8470d7db01e997825d
NorbertCseh <cseh.norbert94@gmail.com>
2022-01-22 10:37:19 +0100

    Generate numbers and display them

--- src/pages/projects/sorting.tsx
+++ src/pages/projects/sorting.tsx
@@ -1,7 +1,22 @@
 import NextPage from "next";
-import { useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 const Sorting = () => {
+  const [numbers, setNumbers] = useState<number[]>([]);
+  const [size, setSize] = useState(5);
+
+  const generateNumbers = useCallback(() => {
+    let rndNumbers: number[] = [];
+    for (let i = 0; i < size; i++) {
+      rndNumbers.push(Math.floor(Math.random() * 300));
+    }
+    setNumbers(rndNumbers);
+  }, [size]);
+
+  useEffect(() => {
+    generateNumbers();
+  }, [generateNumbers]);
+
   return (
     <section className="w-full h-screen p-4">
       <div className="flex h-20 w-full justify-between">
@@ -12,17 +27,29 @@ const Sorting = () => {
             <option value="selection">Selection</option>
             <option value="insertion">Insertion</option>
           </select>
-          <select className="bg-indigo-900" name="size" id="sortin">
+          <select className="bg-indigo-900" name="size" id="size">
+            <option value="5">5</option>
             <option value="15">15</option>
             <option value="20">20</option>
             <option value="25">25</option>
           </select>
           <button>Randomize</button>
+          <button onClick={generateNumbers}>Generate</button>
         </div>
       </div>
       <div>
-        <div className="flex w-full border-2 h-80">
-          Sorting will happen here
+        <div className="flex w-full border-2 h-80 justify-center">
+          {numbers.map((value, index) => {
+            return (
+              <div
+                key={index}
+                className="bg-slate-300 mx-2 mt-2 w-10 text-center text-slate-800"
+                style={{ height: `${value}px` }}
+              >
+                {value}
+              </div>
+            );
+          })}
         </div>
         <div className="border-2 w-full h-24">controll panel</div>
         <div className="w-full h-16 border-2">Color hints</div>
